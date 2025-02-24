b4cf5241c60f2af4d32c804c625cadb6aee40483
AlexMiller93 <alexandr.miller93@gmail.com>
2025-02-24 22:13:14 +0300

    add model Item, urls, empty views

---
+++ items/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('buy/<int:id>/', views.buy_item, name='buy_item'),
+    path('item/<int:id>/', views.item_detail, name='item_detail'),
+]
--- items/views.py
+++ items/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello World!")
+
+
+def buy_item(request, id: int):
+    """ 
+        Метод для покупки предмета Item и получить Stripe Session Id для оплаты выбранного предмета
+    """
+    pass
+
+
+def item_detail(request, id: int):
+    """ 
+        Метод для получения HTML страницы для отображения информации выбранном предмете Item.
+        По нажатию кнопки Buy происходит запрос на url /buy/{id}, получение session_id 
+        и далее с помощью JS библиотеки Stripe происходить редирект на Checkout форму 
+        
+        stripe.redirectToCheckout(sessionId=session_id)
+
+    """
+    pass
