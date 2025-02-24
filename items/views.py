fcad13dfc14a99b8d194fd7137d8bb500688c958
AlexMiller93 <alexandr.miller93@gmail.com>
2025-02-24 22:45:31 +0300

    add views, template, it works

--- items/views.py
+++ items/views.py
@@ -1,7 +1,12 @@
-# Create your views here.
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
 
-from django.http import HttpResponse
+from django.shortcuts import render
+import stripe
 
+from .models import Item
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 def index(request):
     return HttpResponse("Hello World!")
@@ -11,8 +16,29 @@ def buy_item(request, id: int):
     """ 
         Метод для покупки предмета Item и получить Stripe Session Id для оплаты выбранного предмета
     """
-    pass
-
+    try:
+        item = Item.objects.get(id=id)
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'product_data': {
+                            'name': item.name,
+                        },
+                        'unit_amount': int(item.price * 100),
+                    },
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url='http://localhost:8000/success',
+            cancel_url='http://localhost:8000/cancel',
+        )
+        return JsonResponse({'id': session.id})
+    except Item.DoesNotExist:
+        return JsonResponse({'error': 'Item not found'}, status=404)
 
 def item_detail(request, id: int):
     """ 
@@ -23,4 +49,11 @@ def item_detail(request, id: int):
         stripe.redirectToCheckout(sessionId=session_id)
 
     """
-    pass
+    try:
+        item = Item.objects.get(id=id)
+        return render(request, 'item_detail.html', {
+            'item': item,
+            'stripe_publishable_key': settings.STRIPE_PUBLISHABLE_KEY,
+            })
+    except Item.DoesNotExist:
+        return JsonResponse({'error': 'Item not found'}, status=404)
