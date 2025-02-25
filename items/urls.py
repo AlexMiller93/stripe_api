bf397b2fad9e420f3961abefa3935da71ae3766d
AlexMiller93 <alexandr.miller93@gmail.com>
2025-02-25 21:26:22 +0300

    add & test buy_order view

--- items/urls.py
+++ items/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('buy/<int:id>/', views.buy_item, name='buy_item'),
-    path('buy/<int:order_id>/', views.buy_order, name='buy_order'),
     path('item/<int:id>/', views.item_detail, name='item_detail'),
+
+    path('buy_order/<int:order_id>/', views.buy_order, name='buy_order'),
 ]
--- items/views.py
+++ items/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 import stripe
 
-from .models import Item
+from .models import Item, Order
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -60,4 +60,75 @@ def item_detail(request, id: int):
         return JsonResponse({'error': 'Item not found'}, status=404)
     
 def buy_order(request, order_id: int):
-    return HttpResponse(f"Order {order_id} has been successfully processed!")
+    try:
+        order = Order.objects.get(id=order_id)
+        items = order.items.all()
+
+        # Add tax if it exists
+        tax_id = None
+        if order.tax:
+            tax = stripe.TaxRate.create(
+                display_name=order.tax.name,
+                description='Sales Tax',
+                percentage=float(order.tax.percentage),
+                inclusive=False,
+            )
+
+            tax_id = tax.id
+
+        # Add items to the order to the Checkout Session
+        line_items = []
+        for item in items:
+            line_item = {
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': item.name,
+                    },
+                    'unit_amount': int(item.price * 100),
+                },
+                'quantity': 1,
+            }
+
+            # add tax to the line item if it exists
+            if tax_id:
+                line_item['tax_rates'] = [tax_id]
+            line_items.append(line_item)
+
+        # create discount if it exists
+        discount_id = None
+        if order.discount:
+            coupon = stripe.Coupon.create(
+                name=order.discount.name,
+                amount_off=int(order.discount.percentage * 100),
+                currency='usd',
+            )
+
+            discount_id = coupon.id
+
+        # create Stripe Checkout Session
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=line_items,
+            mode='payment',
+            discounts=[{'coupon': discount_id}] if discount_id else None,
+            success_url='http://localhost:8000/success',
+            cancel_url='http://localhost:8000/cancel'
+        )
+
+        return JsonResponse({'id': session.id})
+            
+    except Order.DoesNotExist:
+        return JsonResponse({'error': 'Order not found'}, status=404)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+    
+
+def test_buy_order(request, order_id: int):
+    # if or
+    try:
+        order = Order.objects.get(id=order_id)
+        if order is not None:
+            return HttpResponse(f"Order {order_id} has been successfully processed!")
+    except Order.DoesNotExist:
+        return HttpResponse(f"Oops! ... Order {order_id} no one booked!")
